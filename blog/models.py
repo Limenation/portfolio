d0e1ae92518d4c12ed299dc891220428359d46df
Lime <1448918377@qq.com>
2019-08-13 10:50:01 +0800

    建立blog网站

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+# class Blog(models.Model):
+# 	title = models.CharField(default='文章标题', max_length=50)
+# 	dete = models.DateField()
+# 	image = models.IPAddressField(default='default.png',upload_to = 'images/')
+# 	text = models.TextField(default='正文')
+
+#     def __str__(self):
+
+#     	return self.title
+
+class Blog(models.Model):
+
+    title = models.CharField(default='文章标题', max_length=50)
+    date = models.DateField()
+    image = models.ImageField(default='default.png',upload_to='images/')
+    text = models.TextField(default='正文')
+    def __str__(self):
+        return self.title
+    
